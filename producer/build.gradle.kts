7d6efb600841579ae36b4d014f185f0cbaba6869
enhans-bosung <bosung@enhans.ai>
2025-02-18 18:14:03 +0900

    feat: succeed to export log

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -1,11 +1,25 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+configurations {
+    create("agent")
+}
+
 dependencies {
+    "agent"("io.opentelemetry.javaagent:opentelemetry-javaagent:2.13.0")
+
     implementation(project(":common"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
+tasks.register<Copy>("copyAgent") {
+    from(configurations["agent"]) {
+        rename("opentelemetry-javaagent-.*\\.jar", "opentelemetry-javaagent.jar")
+    }
+    into(layout.buildDirectory.dir("agent"))
+}
+
 tasks.withType<BootJar> {
+    dependsOn("copyAgent")
     archiveFileName = "pct-producer.jar"
 }
